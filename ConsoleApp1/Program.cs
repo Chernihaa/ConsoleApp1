4cbeffa0058d0a7daebfb603f3c3d158cdf77a0a
Chernihaa <Chernihaa@mail.ru>
2025-01-09 23:46:18 +0900

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Здравствуйте, {name}");
+            
+        }
+    }
+}
